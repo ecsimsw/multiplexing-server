07454ea2a8d69162d9282b313db69f8b8fd6419e
ecsimsw <ecsimsw@gmail.com>
2022-04-11 17:23:10 +0900

    feat : change test script

--- k6/script.js
+++ k6/script.js
@@ -1,7 +1,15 @@
 import http from 'k6/http';
-  import { sleep } from 'k6';
-	
-   export default function () {
-   http.get('http://host.docker.internal:8080/userCount');
-   sleep(1);
+import {sleep} from 'k6';
+
+const ROOT_PATH = 'http://host.docker.internal:8080/'
+const SUB_PATH = 'userCount';
+
+export let options = {
+    vus: 1000,
+    duration: '10s',
+};
+
+export default function () {
+    http.get(ROOT_PATH + SUB_PATH);
+    sleep(1);
 }
