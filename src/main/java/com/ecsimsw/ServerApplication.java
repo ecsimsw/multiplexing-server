bdb6b4104b3986e847b93f28eb5f0f2d4aeac833
ecsimsw <ecsimsw@gmail.com>
2022-04-30 05:30:18 +0900

    feat : change endpoint

--- src/main/java/com/ecsimsw/ServerApplication.java
+++ src/main/java/com/ecsimsw/ServerApplication.java
@@ -4,6 +4,7 @@ import com.ecsimsw.server.SingleThreadWebServer;
 import com.ecsimsw.server.WebServer;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 
 import static com.ecsimsw.server.config.ServerConfig.BACK_LOG;
@@ -12,7 +13,8 @@ import static com.ecsimsw.server.config.ServerConfig.PORT;
 public class ServerApplication {
 
     public static void main(String[] args) {
-        final InetSocketAddress endpoint = new InetSocketAddress(PORT);
+        final InetAddress address = null;
+        final InetSocketAddress endpoint = new InetSocketAddress(address, PORT);
 
         try (WebServer webServer = new SingleThreadWebServer()) {
             webServer.init(endpoint, BACK_LOG);
